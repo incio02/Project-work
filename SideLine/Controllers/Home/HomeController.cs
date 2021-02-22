1d6023d95f8e455b208701c0b844dcb3b6dc82be
incio02 <incio.albi02@gmail.com>
2021-02-22 10:58:57 +0100

    Aggiunto layout e inziata la pagina home

---
+++ SideLine/Controllers/Home/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SideLine.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Calcio()
+        {
+            return View();
+        }
+    }
+}
