53e2bba0c1e32ca40f7266fecf00a77b92f6adf6
pietro cilloni <pietro.cilloni@gmail.com>
2021-04-03 17:05:25 +0200

    Aggiunta circuiti e team moto gp(sia a livello strutturale che visivo)

---
+++ SideLine/Controllers/motogp/MotogpController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SideLine.Controllers.motogp
+{
+    public class MotogpController : Controller
+    {
+        [HttpGet]
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+            [HttpGet]
+            public ActionResult Aprilia()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Ducati()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Esponsorama()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult LCRhonda()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Yamaha()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult YamahaSRT()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Pramac()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult KTM()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Honda()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Suzuki()
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult Tech3()
+            {
+                return View();
+            }
+
+        [HttpGet]
+        public ActionResult Circuiti()
+
+        {
+            return View();
+        }
+
+            [HttpGet]
+            public ActionResult GPQatar()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPDoha()
+
+            {
+                return View();
+            }
+            [HttpGet]
+            public ActionResult GPPortogallo()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPSpagna()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPFrancia()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPItalia()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPCatalunya()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPGermania()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPPaesiBassi()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPFinlandia()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPAustria()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPGranBretagna()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPAragon()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPSanMarino()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPGiappone()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPThailandia()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPAustralia()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPMalesia()
+
+            {
+                return View();
+            }
+
+            [HttpGet]
+            public ActionResult GPValencia()
+
+            {
+                return View();
+            }
+    }
+}
